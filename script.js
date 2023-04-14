dc930213a0273ebd1514a84fcac0fb9d9f731871
Gaurav Soni <120136122+gaurav8158@users.noreply.github.com>
2023-04-15 00:47:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,30 +1,63 @@
-//complete this code
+// //complete this code
+// class Rectangle {
+// 	constructor(width,height){
+// this.width= width;
+// 		this.height=height;
+// 	}
+// 	get width(){
+// 		return this.width;
+// 	}
+// 	get height(){
+// 		return this.height;
+// 	}
+// 	getArea(){
+// 		return this.width*this.height;
+// 	}
+// }
+
+// class Square extends Rectangle {
+// 	constructor(side) {
+//     super(side, side);
+//   }
+// 	getPerimeter(){
+// 		var par = 4*width;
+// 		return par;
+// 	}
+// }
+
+
+
 class Rectangle {
-	constructor(width,height){
-this.width= width;
-		this.height=height;
-	}
-	get width(){
-		return this.width;
-	}
-	get height(){
-		return this.height;
-	}
-	getArea(){
-		return this.width*this.height;
-	}
+  constructor(width, height) {
+    this._width = width;
+    this._height = height;
+  }
+  
+  get width() {
+    return this._width;
+  }
+  
+  get height() {
+    return this._height;
+  }
+  
+  getArea() {
+    return this._width * this._height;
+  }
 }
 
 class Square extends Rectangle {
-	constructor(side) {
+  constructor(side) {
     super(side, side);
   }
-	getPerimeter(){
-		var par = 4*width;
-		return par;
-	}
+  
+  getPerimeter() {
+    return 4 * this._width;
+  }
 }
+
 
 // Do not change the code below this line
 window.Rectangle = Rectangle;
 window.Square = Square;
+
